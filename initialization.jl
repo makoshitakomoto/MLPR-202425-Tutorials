385f52ecd7a2ea6b9c0d67bd7be4c4e7013e7db3
Devesh Jawla <devesh.jawla@gmail.com>
2024-09-24 10:48:50 +0100

    Ready for U

--- initialization.jl
+++ initialization.jl
@@ -1,7 +1,5 @@
 import Pkg
-# Pkg.activate("./")
-# Pkg.add(["Pluto", "MLJ", "Flux"])
-
-# Pkg.instantiate()  # Ensure all dependencies listed in Project.toml are installed
-# Pkg.resolve()      # Ensure the package dependency graph is consistent
+Pkg.activate("./")
+Pkg.instantiate()  # Ensure all dependencies listed in Project.toml are installed
+Pkg.resolve()      # Ensure the package dependency graph is consistent
 Pkg.update()       # Fetch the latest versions of your packages
